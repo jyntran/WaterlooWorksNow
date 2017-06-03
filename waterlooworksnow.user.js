87d70e3d08de9cb972cb0c553a0d28ad63b9ff00
Jen Tran <j.yn.tran@gmail.com>
2017-06-02 21:25:12 -0400

    Add expiring icon feature

--- waterlooworksnow.user.js
+++ waterlooworksnow.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WaterlooWorksNow
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      2.0
 // @description  Waterloo Works script to add new functionality. Adds new tab functionality to listings.
 // @author       jyntran
 // @match        https://waterlooworks.uwaterloo.ca/myAccount/hire-waterloo/*
@@ -15,11 +15,10 @@
 
     wwn.openTab = function(fn){
         var newTab = window.open(window.location.href);
-  		var fnBody = fn.toString().match(/function[^{]+\{([\s\S]*)\}$/)[1].trim();
-  		localStorage.setItem("wwnJobOpener", 'window.' + fnBody.toString());
+        var fnBody = fn.toString().match(/function[^{]+\{([\s\S]*)\}$/)[1].trim();
+        localStorage.setItem("wwnJobOpener", 'window.' + fnBody.toString());
         
         newTab.window.onload = function() {
-	    // create loading dialog
             var loadingIconElem = document.createElement('span');
             var loadingElem = document.createElement('div');
             loadingIconElem.setAttribute('class', 'icon-cog icon-spin');
@@ -28,33 +27,50 @@
             loadingElem.setAttribute('style', 'position: absolute; background: black; background: rgba(0,0,0,0.5); width: 100%; height: 100%; top: 0; left: 0; display: flex; align-items: center; justify-content: center; z-index: 1000;');
             loadingElem.appendChild(loadingIconElem);
             newTab.window.document.body.appendChild(loadingElem);
-            // go to listing
+            
             newTab.window.eval(localStorage.wwnJobOpener);
         };
     };
     
+    function renderExpiring() {
+        var expiringListings = $('.isAboutToExpire');
+        
+        for (var i=0; i < expiringListings.length; i++) {
+            var tdElem = expiringListings[i].children[2];
+            var expiringElem = document.createElement('span');
+            expiringElem.setAttribute('class', 'icon-exclamation-sign');
+            expiringElem.setAttribute('style', 'color: #c00; margin: auto 4px');
+            expiringElem.setAttribute('title', 'Deadline today');
+            tdElem.appendChild(expiringElem);
+        }
+    }
+    
     function renderNewTabButtons() {                    
-	    var searchResults = document.getElementsByClassName('searchResult');
+        var searchResults = document.getElementsByClassName('searchResult');
 
-	    for (var i=0; i < searchResults.length; i++) {
+        for (var i=0; i < searchResults.length; i++) {
             var tdElem = searchResults[i].children[2];
-	        var linkElem = tdElem.getElementsByTagName('a')[0];
-	        var onclickContents = linkElem.onclick;
-	        var newElem = document.createElement('button');
-	        var newSpan = document.createElement('span');
-	        newSpan.setAttribute('class', 'icon-external-link');
-	        newElem.setAttribute('onclick', 'wwn.openTab('+ onclickContents +');');
-	        newElem.setAttribute('class', 'btn btn-mini wwn-newtab');
-            	newElem.setAttribute('title', 'Open in new tab');
-	        newElem.appendChild(newSpan);
-	        tdElem.appendChild(newElem);
-	    }
+            var linkElem = tdElem.getElementsByTagName('a')[0];
+            var onclickContents = linkElem.onclick;
+            var newElem = document.createElement('button');
+            var newSpan = document.createElement('span');
+            newSpan.setAttribute('class', 'icon-external-link');
+            newElem.setAttribute('onclick', 'wwn.openTab('+ onclickContents +');');
+            newElem.setAttribute('class', 'btn btn-mini wwn-newtab');
+            newElem.setAttribute('style', 'margin: auto 4px');
+            newElem.setAttribute('title', 'Open in new tab');
+            newElem.appendChild(newSpan);
+            tdElem.appendChild(newElem);
+        }
         
         // remove br element after job title link
         $('.searchResult td:nth-child(' + 3 + ') br').remove();
     }
     
     function renderWWN() {
+        if ($('.isAboutToExpire').length) {
+            renderExpiring();
+        }
         if (!$('.wwn-newtab').length) {
             renderNewTabButtons();
         }
